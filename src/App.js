ca5bdf3155d910d4c08e67353d10f0c17a26ee94
Yukti Jhawar <yuktimj2002@gmail.com>
2023-05-26 00:08:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [temperatureValue,setTemperatureValue] = useState(10);
+  const [color,setColor] = useState('normal');
+  const [image,setImage] = useState('normalImage');
+
+  const increaseFunc=()=>{
+    var newTemp = temperatureValue + 1;
+
+    if(newTemp>0 && newTemp<20){
+      setColor('normal')
+      setImage('normalImage')
+    }else if(newTemp>-20 && newTemp<0){
+      setColor('cold')
+      setImage('coldImage')
+    }else if(newTemp<-20){
+      setColor('veryCold')
+      setImage('veryColdImage')
+    }else if(newTemp>20 && newTemp<40){
+      setColor('hot')
+      setImage('hotImage')
+    }else if(newTemp>40){
+      setColor('veryHot')
+      setImage('veryHotImage')
+    }
+
+    setTemperatureValue(newTemp);
+  }
+  const decreaseFunc=()=>{
+    var newTemp = temperatureValue - 1;
+
+    if(newTemp>0 && newTemp<20){
+      setColor('normal')
+      setImage('normalImage')
+    }else if(newTemp>-20 && newTemp<0){
+      setColor('cold')
+      setImage('coldImage')
+    }else if(newTemp<-20){
+      setColor('veryCold')
+      setImage('veryColdImage')
+    }else if(newTemp>20 && newTemp<40){
+      setColor('hot')
+      setImage('hotImage')
+    }else if(newTemp>40){
+      setColor('veryHot')
+      setImage('veryHotImage')
+    }
+
+    setTemperatureValue(newTemp);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${image}`}>
+    <div className='temperature-container'>
+      <div className='temperature-card'>
+        <div className='temperature-value-container'>
+        <div className={`temperature-value ${color}`}>{temperatureValue}&deg;C</div>
+        </div>
+        <div className='button-container'>
+        <button className='increase button' onClick={increaseFunc}>+</button>
+        <button className='decrease button' onClick={decreaseFunc}>-</button>
+        </div>
+      </div>
     </div>
-  );
+    </div>
+    );
 }
 
 export default App;
+
+/* <h1>&deg;</h1>
+        <h1>&#176;</h1>
+        <h1>&#x00B0;</h1>
+        <h1>&#xB0;</h1> {/* for hex code, you can omit the leading zeros */
+        /* <h1>{'\u00B0'}</h1> */
